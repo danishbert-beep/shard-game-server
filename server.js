908c21e7e81a0e961da771586741bea9683381b8
danishbert-beep <danishbert@gmail.com>
2026-03-10 00:36:51 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -26,6 +26,35 @@ function getAvailablePlayerNum() {
   return null;
 }
 
+// -------------------------
+// shared moving machines
+// -------------------------
+const sideMachineMover = {
+  machineIds: [6, 7],
+  amplitude: 45,
+  speed: 0.03,
+  phase: Math.PI,
+  t: 0
+};
+
+function buildMachineOffsets() {
+  const machineOffsets = {};
+
+  sideMachineMover.machineIds.forEach((id, index) => {
+    const phase = index === 0 ? 0 : sideMachineMover.phase;
+    machineOffsets[id] =
+      Math.sin(sideMachineMover.t + phase) * sideMachineMover.amplitude;
+  });
+
+  return machineOffsets;
+}
+
+// broadcast shared machine state
+setInterval(() => {
+  sideMachineMover.t += sideMachineMover.speed;
+  io.emit("machineOffsets", buildMachineOffsets());
+}, 1000 / 60);
+
 io.on("connection", socket => {
   const playerNum = getAvailablePlayerNum();
 
@@ -38,17 +67,24 @@ io.on("connection", socket => {
   players[socket.id] = {
     playerNum,
     x: 0,
-    y: 0
+    y: 0,
+    topLeftX: 0,
+    topLeftY: 0,
+    attacking: false
   };
 
   socket.emit("assignPlayerNum", playerNum);
   socket.emit("currentPlayers", players);
+  socket.emit("machineOffsets", buildMachineOffsets());
 
   socket.broadcast.emit("playerJoined", {
     id: socket.id,
     playerNum,
     x: 0,
-    y: 0
+    y: 0,
+    topLeftX: 0,
+    topLeftY: 0,
+    attacking: false
   });
 
   socket.on("move", data => {
@@ -63,6 +99,18 @@ io.on("connection", socket => {
       id: socket.id,
       playerNum: players[socket.id].playerNum,
       ...data
+    });
+  });
+
+  socket.on("attackState", data => {
+    if (!players[socket.id]) return;
+
+    players[socket.id].attacking = !!data.attacking;
+
+    socket.broadcast.emit("playerAttackState", {
+      id: socket.id,
+      playerNum: players[socket.id].playerNum,
+      attacking: players[socket.id].attacking
     });
   });
 
